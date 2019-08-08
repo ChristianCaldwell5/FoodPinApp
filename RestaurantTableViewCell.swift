6b10d9c5906aeda9b8e9165196bef59866ff86ca
ChristianCaldwell5 <christian.c2969@gmail.com>
2019-08-08 17:26:33 -0500

    Transfer code to git

---
+++ RestaurantTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Christian C on 5/21/18.
+//  Copyright © 2018 SegFaultZero. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView! 
+    @IBOutlet var heartImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
